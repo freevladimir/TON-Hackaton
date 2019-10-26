d0a99f68d7adf119964de047918552314d29fbad
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-10-26 15:46:25 +0300

    add body parser

--- blockchain-api/index.js
+++ blockchain-api/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { exec } = require("child_process");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log("Server ready");
